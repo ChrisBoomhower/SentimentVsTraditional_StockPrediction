23dcc124a618397dcf40e5509264035f239c9155
Chris Boomhower <cboomhower@smu.edu>
2018-03-16 00:32:19 -0500

    ProcessMessages.R mostly complete. Just need to add conversion of timestamps to a time series type and finalize outputs to Tableau.

--- Analysis/Data/ProcessMessages.R
+++ Analysis/Data/ProcessMessages.R
@@ -9,7 +9,13 @@ require(jsonlite)
 require(stringr)
 require(dplyr)
 
-## Consolidate StockTwits ticker symbol data into single dataframes with duplicates dropped
+Tableau = FALSE #Set conditional term for processing word dataframes
+
+#################################
+###### DATA CONSOLIDATION #######
+#################################
+
+# ## Consolidate StockTwits ticker symbol data into single dataframes with duplicates dropped
 setwd("C:/Users/Owner/Documents/GitHub/MSDS_8390/SentimentVsTraditional_StockPrediction/Analysis/Data/StockTwits")
 ST.tickers <- list.files(pattern = '[^getStockTwits.R|getStockTwits.bat|getStockTwits.Rout]')
 
@@ -23,12 +29,12 @@ for(s in ST.tickers){
     dat_list <- lapply(files, readRDS)
     dat_list <- lapply(dat_list, flatten) #Need to flatten dataframes since dfs from JSON contain nested dfs
     df <- ldply(dat_list, data.frame) #Combine list dataframes into single dataframe
-    
+
     ## Remove duplicates; of duplicates, keep only record with highest likes.total
     temp <- df[!duplicated(df[,cols.u]),]
     temp$likes.total <- ifelse(is.na(temp$likes.total), 0, temp$likes.total) #Fill NAs with 0
     df <- temp %>% group_by(id, body, created_at, user.id) %>% top_n(1, likes.total) #Inspired by https://stackoverflow.com/questions/24558328/how-to-select-the-row-with-the-maximum-value-in-each-group
-    
+
     assign(paste0(s,".ST"), df)
 
     setwd('..')
@@ -45,17 +51,17 @@ T.tickers <- list.files(pattern = '[^getTweets.R|getTweets.bat|getTweets.Rout]')
 cols.u <- c("id", "text", "created", "screenName", "favoriteCount")
 
 start <- Sys.time() #Start timer
-for(s in T.tickers[]){
+for(s in T.tickers){
     setwd(s)
 
     files <- list.files(pattern = '\\.RDS$')
     dat_list <- lapply(files, readRDS)
     df <- ldply(dat_list, data.frame)
-    
+
     ## Remove duplicates; of duplicates, keep only record with highest favoriteCount
     temp <- df[!duplicated(df[,cols.u]),]
     df <- temp %>% group_by(id, text, created, screenName) %>% top_n(1, favoriteCount) #Inspired by https://stackoverflow.com/questions/24558328/how-to-select-the-row-with-the-maximum-value-in-each-group
-    
+
     assign(paste0(s,".T"), df)
 
     setwd('..')
@@ -64,23 +70,36 @@ rm(df)
 rm(temp)
 rm(dat_list)
 print(Sys.time() - start)
-# 
-# ## Consolidate YahooFinance ticker symbol data into single dataframes with duplicates dropped
-# setwd("C:/Users/Owner/Documents/GitHub/MSDS_8390/SentimentVsTraditional_StockPrediction/Analysis/Data/YahooFinance")
-# YF.tickers <- list.files(pattern = '[^getNews.R|getNews.bat|getNews.Rout]')
-# for(s in YF.tickers){
-#     setwd(s)
-#     
-#     files <- list.files(pattern = '\\.RDS$')
-#     dat_list <- lapply(files, readRDS)
-#     df <- ldply(dat_list, data.frame)
-#     assign(paste0(s,".YF"), unique(df)) #Drop duplicates and assign to variable named after ticker symbol
-#     
-#     setwd('..')
-# }
-# rm(df)
 
-## Generate sentiment scores
+## Consolidate YahooFinance ticker symbol data into single dataframes with duplicates dropped
+setwd("C:/Users/Owner/Documents/GitHub/MSDS_8390/SentimentVsTraditional_StockPrediction/Analysis/Data/YahooFinance")
+YF.tickers <- list.files(pattern = '[^getNews.R|getNews.bat|getNews.Rout]')
+
+start <- Sys.time() #Start timer
+for(s in YF.tickers){
+    setwd(s)
+
+    files <- list.files(pattern = '\\.RDS$')
+    dat_list <- lapply(files, readRDS)
+    df <- ldply(dat_list, data.frame)
+
+    df <- df[!duplicated(df$description),]
+    assign(paste0(s,".YF"), df) #Drop duplicates and assign to variable named after ticker symbol
+
+    setwd('..')
+}
+rm(df)
+rm(dat_list)
+print(Sys.time() - start)
+
+#################################
+####### SCORING FUNCTIONS #######
+#################################
+
+## Get good and bad words
+## See Minqing Hu and Bing Liu papers titled,
+## "Mining and Summarizing Customer Reviews" and
+## "Opinion Observer: Analyzing and Comparing Opinions on the Web,"
 setwd("..")
 positive = scan('positive-words.txt', what='character', comment.char=';') #Produce good word vector
 negative = scan('negative-words.txt', what='character', comment.char=';') #Produce negative word vector
@@ -89,47 +108,52 @@ positive = c(positive, 'wtf', 'epicfail', 'bearish', 'bear')
 negative = c(negative, 'upgrade', ':)', 'bullish', 'bull')
 
 ## Following function modified from https://github.com/jeffreybreen/twitter-sentiment-analysis-tutorial-201107/blob/master/R/sentiment.R
-score.sentiment = function(sentences, pos.words, neg.words, feed, addition = '', .progress='none'){
+make.score <- function(sentence, pos.words, neg.words, cloud) {
+    
+    ## clean up sentence contents:
+    sentence = gsub('[[:punct:]]', '', sentence)
+    sentence = gsub('[[:cntrl:]]', '', sentence)
+    sentence = gsub('\\d+', '', sentence)
+    
+    ## convert to lower case:
+    sentence = try(tolower(sentence))
+    
+    ## split into words and reduce list of single list object to just list
+    word.list = str_split(sentence, '\\s+')
+    words = unlist(word.list)
+    
+    ## compare message words against good and bad word dictionaries
+    pos.matches = match(words, pos.words)
+    neg.matches = match(words, neg.words)
+    
+    ## get indices of matched terms
+    pos.matches = !is.na(pos.matches)
+    neg.matches = !is.na(neg.matches)
+    
+    ## calculate score based on summations
+    score = sum(pos.matches) - sum(neg.matches)
+    
+    if(cloud) return(list(words, pos.matches, neg.matches)) #Use when generating df for Tableau wordcloud
+    else return(score) #Use when getting scores
+}
+
+score.sentiment = function(sentences, pos.words, neg.words, feed, addition = '', cloud = FALSE, .progress='none'){
     if(feed == 'ST'){
         sentences <- ifelse(addition == '', sentences, paste(sentences, addition)) #Tag on additional column content when needed
     }
     sentences <- try(iconv(sentences, 'UTF-8', 'latin1')) #Inspired by https://stackoverflow.com/questions/9637278/r-tm-package-invalid-input-in-utf8towcs
-    #print(addition)
-    #print(sentences)
+
     # we got a vector of sentences. plyr will handle a list or a vector as an "l" for us
     # we want a simple array of scores back, so we use "l" + "a" + "ply" = laply:
-    scores = laply(sentences, function(sentence, pos.words, neg.words) {
-        
-        # clean up sentences with R's regex-driven global substitute, gsub():
-        sentence = gsub('[[:punct:]]', '', sentence)
-        sentence = gsub('[[:cntrl:]]', '', sentence)
-        sentence = gsub('\\d+', '', sentence)
-        # and convert to lower case:
-        sentence = try(tolower(sentence))
-        
-        # split into words. str_split is in the stringr package
-        word.list = str_split(sentence, '\\s+')
-        # sometimes a list() is one level of hierarchy too much
-        words = unlist(word.list)
-        
-        # compare our words to the dictionaries of positive & negative terms
-        pos.matches = match(words, pos.words)
-        neg.matches = match(words, neg.words)
-        
-        # match() returns the position of the matched term or NA
-        # we just want a TRUE/FALSE:
-        pos.matches = !is.na(pos.matches)
-        neg.matches = !is.na(neg.matches)
-        
-        # and conveniently enough, TRUE/FALSE will be treated as 1/0 by sum():
-        score = sum(pos.matches) - sum(neg.matches)
-        
-        return(score)
-    }, pos.words, neg.words, .progress=.progress )
+    scores = laply(sentences, make.score, pos.words, neg.words, cloud, .progress=.progress )
     
     scores.df = data.frame(score=scores, text=sentences)
     return(scores.df)
 }
+
+#################################
+######## GENERATE SCORES ########
+#################################
 
 ## Generate StockTwits scores
 ST <- ls(pattern = '.\\.ST$') #Get list of StockTwits objects
@@ -137,54 +161,149 @@ for(s in ST){
     temp <- score.sentiment(get(s)[["body"]], positive, negative, feed = 'ST',
                             addition = get(s)[["entities.sentiment.basic"]],
                             .progress='text')
-    
+
     ## Multiply score by likes.total and add score column to social dataframes
     assign(paste0(s,".scores"), temp)
-    assign(s, `[[<-`(get(s), 'score', #New variable assignment syntax inspired by https://stackoverflow.com/questions/15670193/how-to-use-assign-or-get-on-specific-named-column-of-a-dataframe
+    assign(s, `[[<-`(get(s), 'scoreLikes', #New variable assignment syntax inspired by https://stackoverflow.com/questions/15670193/how-to-use-assign-or-get-on-specific-named-column-of-a-dataframe
                      value =ifelse(get(s)[["likes.total"]] > 0,
                                    temp[,1] * get(s)[["likes.total"]],
                                    temp[,1])))
+
+    ## Also add raw scores to social dataframes
+    assign(s, `[[<-`(get(s), 'score', #New variable assignment syntax inspired by https://stackoverflow.com/questions/15670193/how-to-use-assign-or-get-on-specific-named-column-of-a-dataframe
+                     value =temp[,1]))
 }
 rm(temp)
-
-# genScores(ST, "body", "entities.sentiment.basic", "likes.total")
-# 
-# genScores <- function(obj, main, adder, multi){
-#     for(s in obj){
-#         temp <- score.sentiment(get(s)[[main]], positive, negative,
-#                                 addition = get(s)[[adder]],
-#                                 .progress='text')
-#         
-#         ## Multiply score by likes.total and add score column to social dataframes
-#         assign(paste0(s,".scores"), temp)
-#         assign(s, `[[<-`(get(s), 'score', #New variable assignment syntax inspired by https://stackoverflow.com/questions/15670193/how-to-use-assign-or-get-on-specific-named-column-of-a-dataframe
-#                          value =ifelse(get(s)[[multi]] > 0,
-#                                        temp[,1] * get(s)[["multi"]],
-#                                        temp[,1])))
-#     }
-#     rm(temp)
-# }
 
 #FOLLOWING LINE FOR DEBUG ONLY
 #temp <- score.sentiment(AAPL.ST[["body"]], positive, negative, addition = AAPL.ST[,"entities.sentiment.basic"], .progress='text')
 
 ## Generate Twitter scores
-T <- ls(pattern = '.\\.T$') #Get list of Twitter objects
+Tw <- ls(pattern = '.\\.T$') #Get list of Twitter objects
 start <- Sys.time() #Start timer
-for(s in T){
+for(s in Tw){
     temp <- score.sentiment(get(s)[["text"]], positive, negative,
-                            feed = "T", addition = "",
+                            feed = "Tw", addition = "",
                             .progress='text')
-    
+
     ## Multiply score by likes.total and add score column to social dataframes
     assign(paste0(s,".scores"), temp)
-    assign(s, `[[<-`(get(s), 'score', #New variable assignment syntax inspired by https://stackoverflow.com/questions/15670193/how-to-use-assign-or-get-on-specific-named-column-of-a-dataframe
+    assign(s, `[[<-`(get(s), 'scoreLikes',
                      value =ifelse(get(s)[["favoriteCount"]] > 0,
                                    temp[,1] * get(s)[["favoriteCount"]],
                                    temp[,1])))
+
+    ## Also add raw scores to social dataframes
+    assign(s, `[[<-`(get(s), 'score',
+                     value =temp[,1]))
 }
 rm(temp)
 print(Sys.time() - start)
 
 #FOLLOWING LINE FOR DEBUG ONLY
-#temp <- score.sentiment(AAPL.T[["text"]], positive, negative, feed = "T", .progress='text')
+#temp <- score.sentiment(AAPL.T[["text"]], positive, negative, feed = "Tw", .progress='text')
+
+## Generate Yahoo Finance scores
+YF <- ls(pattern = '.\\.YF$') #Get list of Twitter objects
+start <- Sys.time() #Start timer
+for(s in YF){
+    temp <- score.sentiment(get(s)[["description"]], positive, negative,
+                            feed = "YF", addition = "",
+                            .progress='text')
+    
+    ## Add raw scores to social dataframes
+    assign(s, `[[<-`(get(s), 'score',
+                     value =temp[,1]))
+}
+rm(temp)
+print(Sys.time() - start)
+
+#FOLLOWING LINE FOR DEBUG ONLY
+#temp <- score.sentiment(AAPL.YF[["description"]], positive, negative, feed = "YF", addition = "", .progress='text')
+
+## Cleanup unneeded objects
+rm(list=ls(pattern = '.\\.scores$'))
+rm(cols.u, files, s, ST.tickers, start, T.tickers, YF.tickers)
+
+######################################
+####### GET WORDS FOR TABLEAUE #######
+######################################
+
+if(Tableau){
+    ## Word generation wrapper function
+    genWords <- function(feed, message, df){
+        start <- Sys.time() #Start timer
+        for(s in feed){
+            print(s)
+            sentences <- try(iconv(get(s)[[message]], 'UTF-8', 'latin1'))
+            cloud.D <- lapply(sentences, make.score, positive, negative, cloud = TRUE)
+            temp <- ldply(cloud.D, data.frame)
+            
+            W <- sapply(cloud.D, function(x) x[1])
+            P <- sapply(cloud.D, function(x) x[2])
+            N <- sapply(cloud.D, function(x) x[3])
+            temp <- data.frame(Word = unlist(W), Good = unlist(P), Bad = unlist(N), TickerSource = s)
+            temp$Dict <- ifelse(temp$Good == TRUE, "Good",
+                                ifelse(temp$Bad == TRUE, "Bad", "Neutral"))
+            
+            df = rbind(df, temp)
+        }
+        rm(temp,W,P,N,cloud.D)
+        print(Sys.time() - start)
+        return(df)
+    }
+    
+    ## Extract StockTwits words
+    words.ST <- data.frame(Word = factor(), Good = logical(), Bad = logical(), TickerSource = character(), Dict = character())
+    words.ST <- genWords(ST, "body", words.ST)
+    
+    
+    # for(s in ST[1]){
+    #     sentences <- try(iconv(get(s)[["body"]], 'UTF-8', 'latin1'))
+    #     cloud.D <- lapply(sentences, make.score, positive, negative, cloud = TRUE)
+    #     temp <- ldply(cloud.D, data.frame)
+    #     
+    #     W <- sapply(cloud.D, function(x) x[1])
+    #     P <- sapply(cloud.D, function(x) x[2])
+    #     N <- sapply(cloud.D, function(x) x[3])
+    #     temp <- data.frame(Word = unlist(W), Good = unlist(P), Bad = unlist(N), TickerSource = s)
+    #     temp$Dict <- ifelse(temp$Good == TRUE, "Good",
+    #                         ifelse(temp$Bad == TRUE, "Bad", "Neutral"))
+    #     
+    #     words.ST = rbind(words.YF, temp)
+    # }
+    
+    ## Extract Yahoo Finance words
+    words.YF <- data.frame(Word = factor(), Good = logical(), Bad = logical(), TickerSource = character(), Dict = character())
+    words.YF <- genWords(YF, "description", words.YF)
+    
+    
+    ## Extract Twitter words (Extract Twitter data last so we can drop SF and YF data first)
+    rm(list=ls(pattern = '.\\.ST$'))
+    rm(list=ls(pattern = '.\\.YF$'))
+    
+    words.T <- data.frame(Word = factor(), Good = logical(), Bad = logical(), TickerSource = character(), Dict = character())
+    Tw.sym = c("AAPL.T", "AMZN.T", "Boeing.T", "DowDuPont.T", "Johnson&Johnson.T",
+               "JPM.T", "NEE.T", "Proctor&Gamble.T", "SPG.T", "Verizon.T", "Exxon.T")
+    words.T <- genWords(Tw.sym, "text", words.T)
+    
+    allWords <- rbind(words.ST, words.T, words.YF)
+    
+    # start <- Sys.time() #Start timer
+    # for(s in YF){
+    #     cloud.D <- lapply(get(s)[["description"]], make.score, positive, negative, cloud = TRUE)
+    #     temp <- ldply(cloud.D, data.frame)
+    #     
+    #     W <- sapply(cloud.D, function(x) x[1])
+    #     P <- sapply(cloud.D, function(x) x[2])
+    #     N <- sapply(cloud.D, function(x) x[3])
+    #     temp <- data.frame(Word = unlist(W), Good = unlist(P), Bad = unlist(N), TickerSource = s)
+    #     temp$Dict <- ifelse(temp$Good == TRUE, "Good",
+    #                         ifelse(temp$Bad == TRUE, "Bad", "Neutral"))
+    #     
+    #     words.YF = rbind(words.YF, temp)
+    # }
+    # write.csv(words.YF, "Tableau/words.YF.csv")
+    # rm(temp,W,P,N)
+    # print(Sys.time() - start)
+}
